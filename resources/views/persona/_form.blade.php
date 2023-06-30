d4d82a0a6ec2d81dcaf151037a387540216326d3
bdtem07477 <miguel.godoy@bt.gob.ve>
2023-06-30 18:03:53 -0400

    fix( views/persona )

--- resources/views/persona/_form.blade.php
+++ resources/views/persona/_form.blade.php
@@ -1,9 +1,11 @@
 <div class="form-group row">
     <label for="estatu" class="col-sm-2 col-form-label">Estatus</label>
     <div class="col-sm-8">
-        <select class="form-control select" name="estatu" style="width: 100%;">
-            <option value="1" selected="selected">Activo</option>
-            <option value="0">Inactivo</option>
+        <select class="form-control select" name="estatu" style="width: 100%;" placeholder="-- Seleccione --">
+            
+            @foreach($estatus as $estatu)
+            <option value="{{ $estatu->id }}" {{ (isset($estatu->id) || old('id'))}}> {{ $estatu->estatu }}</option>
+            @endforeach
         </select>
     </div>
 </div>
@@ -16,10 +18,10 @@
                     <i class="far fa-address-card"></i>
                 </span>
             </div>
-            <input type="text" name="ci" class="form-control @error('ci') is-invalid @enderror" placeholder="xxxxxxxx" autocomplete="off">
-            <div class="invalid-feedback">
+            <input type="text" name="ci" class="form-control @error('ci') is-invalid @enderror" placeholder="xxxxxxxx" value="{{ old('ci') }}" autocomplete="off">
+            <div class="invalid-feedback" role="alert">
                 @error('ci')
-                <h6>{{ $message }}</h6>
+                <strong>{{ $message }}</strong>
                 @enderror
             </div>
         </div>
@@ -34,10 +36,10 @@
                     <i class="fas fa-user"></i>
                 </span>
             </div>
-            <input type="text" name="usuario" class="form-control @error('usuario') is-invalid @enderror" placeholder="bdtexxxxxx" autocomplete="off">
-            <div class="invalid-feedback">
+            <input type="text" name="usuario" class="form-control @error('usuario') is-invalid @enderror" placeholder="bdtexxxxxx" value="{{ old('usuario') }}" autocomplete="off">
+            <div class="invalid-feedback" role="alert">
                 @error('usuario')
-                <h6>{{ $message }}</h6>
+                <strong>{{ $message }}</strong>
                 @enderror
             </div>
         </div>
@@ -52,11 +54,11 @@
                     <i class="fas fa-grin"></i>
                 </span>
             </div>
-            <input type="text" name="pnombre" class="form-control @error('pnombre') is-invalid @enderror" placeholder="Primer Nombre" autocomplete="off">
-            <input type="text" name="snombre" class="form-control" placeholder="Segundo Nombre" autocomplete="off">
-            <div class="invalid-feedback">
+            <input type="text" name="pnombre" class="form-control @error('pnombre') is-invalid @enderror" value="{{ old('pnombre') }}" placeholder="Primer Nombre" autocomplete="off">
+            <input type="text" name="snombre" class="form-control" placeholder="Segundo Nombre" value="{{ old('snombre') }}" autocomplete="off">
+            <div class="invalid-feedback" role="alert">
                 @error('pnombre')
-                <h6>{{ $message }}</h6>
+                <strong>{{ $message }}</strong>
                 @enderror
             </div>
         </div>
@@ -71,11 +73,11 @@
                     <i class="fas fa-grin"></i>
                 </span>
             </div>
-            <input type="text" name="papellido" class="form-control @error('pnombre') is-invalid @enderror" placeholder="Primer Apellido" autocomplete="off">
-            <input type="text" name="sapellido" class="form-control" placeholder="Segundo Apellido" autocomplete="off">
-            <div class="invalid-feedback">
+            <input type="text" name="papellido" class="form-control @error('papellido') is-invalid @enderror" value="{{ old('papellido') }}" placeholder="Primer Apellido" autocomplete="off">
+            <input type="text" name="sapellido" class="form-control" value="{{ old('sapellido') }}" placeholder="Segundo Apellido" autocomplete="off">
+            <div class="invalid-feedback" role="alert">
                 @error('papellido')
-                <h6>{{ $message }}</h6>
+                <strong>{{ $message }}</strong>
                 @enderror
             </div>
         </div>
@@ -85,9 +87,10 @@
     <label for="genero" class="col-sm-2 col-form-label">Genero</label>
     <div class="col-sm-8">
         <select class="form-control select" name="genero" style="width: 100%;">
-            <option value="" selected="selected">Femenino</option>
-            <option value="">Masculino</option>
-            <option value="">Otros</option>
+            <option value="" selected>-- Seleccione --</option>
+            @foreach($generos as $genero)
+            <option value="{{ $genero->id }}" {{ (isset($genero->id) || old('id'))}}>{{ $genero->genero }}</option>
+            @endforeach
         </select>
     </div>
 </div>
@@ -100,10 +103,10 @@
                     <i class="fas fa-camera"></i>
                 </span>
             </div>
-            <input type="file" name="foto" class="form-control @error('foto') is-invalid @enderror">
-            <div class="invalid-feedback">
+            <input type="file" name="foto" class="form-control @error('foto') is-invalid @enderror" value="{{ old('foto') }}">
+            <div class="invalid-feedback" role="alert">
                 @error('foto')
-                <h6>{{ $message }}</h6>
+                <strong>{{ $message }}</strong>
                 @enderror
             </div>
         </div>
--- resources/views/persona/index.blade.php
+++ resources/views/persona/index.blade.php
@@ -3,243 +3,84 @@
 @section('title', 'SIGECOM')
 
 @section('content_header')
-	<br>
-	<h1 class="m-0 text-dark">
-		Gestión de personas
-		<small class="font-weight-light ml-1 text-md">Lista de personas</small>
-	</h1>
-	<br>
+<br>
+<h1 class="m-0 text-dark">
+	Gestión de personas
+	<small class="font-weight-light ml-1 text-md">Lista de personas</small>
+</h1>
+<br>
 @stop
 
 @section('content')
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-12">
-				<div class="card">
-					<div class="card-header">
-						<div class="card-tools">
-							<div class="btn-group">
-								<a href="{{ route('persona.create') }}" class="btn btn-sm btn-block btn-primary">
-									<i class="fa fa-plus"></i>
-									Agregar persona
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class="card-body">
-						<div class="row">
-							<div class="col-sm-3">
-								<div class="card card-widget widget-user">
-									<div class="widget-user-header bg-info">
-										<a href="{{ route('persona.show', $persona=1) }}">
-										<h3 class="widget-user-username">Persona 1</h3>
-										</a>
-										<h5 class="widget-user-desc">Founder &amp; CEO</h5>
-									</div>
-									<div class="widget-user-image">
-										<img class="img-circle elevation-2" src="vendor/adminlte/dist/img/avatar.png" alt="User Avatar">
-									</div>
-									<div class="card-footer">
-										<div class="row">
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">3,200</h5>
-													<span class="description-text">SALES</span>
-												</div>
-											</div>
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">13,000</h5>
-													<span class="description-text">FOLLOWERS</span>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="description-block">
-													<h5 class="description-header">35</h5>
-													<span class="description-text">PRODUCTS</span>
-												</div>
-											</div>
-										</div>
-								</div>
-								</div>
-							</div>
-							<div class="col-sm-3">
-								<div class="card card-widget widget-user">
-									<div class="widget-user-header bg-info">
-										<h3 class="widget-user-username">Persona 2</h3>
-										<h5 class="widget-user-desc">Founder &amp; CEO</h5>
-									</div>
-									<div class="widget-user-image">
-										<img class="img-circle elevation-2" src="vendor/adminlte/dist/img/avatar2.png" alt="User Avatar">
-									</div>
-									<div class="card-footer">
-										<div class="row">
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">3,200</h5>
-													<span class="description-text">SALES</span>
-												</div>
-											</div>
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">13,000</h5>
-													<span class="description-text">FOLLOWERS</span>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="description-block">
-													<h5 class="description-header">35</h5>
-													<span class="description-text">PRODUCTS</span>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3">
-								<div class="card card-widget widget-user">
-									<div class="widget-user-header bg-info">
-										<h3 class="widget-user-username">Persona 3</h3>
-										<h5 class="widget-user-desc">Founder &amp; CEO</h5>
-									</div>
-									<div class="widget-user-image">
-										<img class="img-circle elevation-2" src="vendor/adminlte/dist/img/avatar3.png" alt="User Avatar">
-									</div>
-									<div class="card-footer">
-										<div class="row">
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">3,200</h5>
-													<span class="description-text">SALES</span>
-												</div>
-											</div>
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">13,000</h5>
-													<span class="description-text">FOLLOWERS</span>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="description-block">
-													<h5 class="description-header">35</h5>
-													<span class="description-text">PRODUCTS</span>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3">
-								<div class="card card-widget widget-user">
-									<div class="widget-user-header bg-info">
-										<h3 class="widget-user-username">Persona 4</h3>
-										<h5 class="widget-user-desc">Founder &amp; CEO</h5>
-									</div>
-									<div class="widget-user-image">
-										<img class="img-circle elevation-2" src="vendor/adminlte/dist/img/avatar4.png" alt="User Avatar">
-									</div>
-									<div class="card-footer">
-										<div class="row">
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">3,200</h5>
-													<span class="description-text">SALES</span>
-												</div>
-											</div>
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">13,000</h5>
-													<span class="description-text">FOLLOWERS</span>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="description-block">
-													<h5 class="description-header">35</h5>
-													<span class="description-text">PRODUCTS</span>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3">
-								<div class="card card-widget widget-user">
-									<div class="widget-user-header bg-info">
-										<h3 class="widget-user-username">Persona 5</h3>
-										<h5 class="widget-user-desc">Founder &amp; CEO</h5>
-									</div>
-									<div class="widget-user-image">
-										<img class="img-circle elevation-2" src="vendor/adminlte/dist/img/avatar5.png" alt="User Avatar">
-									</div>
-									<div class="card-footer">
-										<div class="row">
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">3,200</h5>
-													<span class="description-text">SALES</span>
-												</div>
-											</div>
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">13,000</h5>
-													<span class="description-text">FOLLOWERS</span>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="description-block">
-													<h5 class="description-header">35</h5>
-													<span class="description-text">PRODUCTS</span>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3">
-								<div class="card card-widget widget-user">
-									<div class="widget-user-header bg-info">
-										<h3 class="widget-user-username">Persona 6</h3>
-										<h5 class="widget-user-desc">Founder &amp; CEO</h5>
-									</div>
-									<div class="widget-user-image">
-										<img class="img-circle elevation-2" src="vendor/adminlte/dist/img/avatar.png" alt="User Avatar">
-									</div>
-									<div class="card-footer">
-										<div class="row">
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">3,200</h5>
-													<span class="description-text">SALES</span>
-												</div>
-											</div>
-											<div class="col-sm-4 border-right">
-												<div class="description-block">
-													<h5 class="description-header">13,000</h5>
-													<span class="description-text">FOLLOWERS</span>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="description-block">
-													<h5 class="description-header">35</h5>
-													<span class="description-text">PRODUCTS</span>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+			<div class="card">
+				<div class="card-header">
+					<div class="card-tools">
+						<div class="btn-group">
+							<a href="{{ route('persona.create') }}" class="btn btn-sm btn-block btn-primary">
+								<i class="fa fa-plus"></i>
+								Agregar persona
+							</a>
 						</div>
 					</div>
 				</div>
+				<div class="card-body">
+					<div class="row">
+						@foreach($personas as $persona)
+						<?php //var_dump($persona->foto)?>
+						<div class="col-sm-3">
+							<div class="card card-widget widget-user">
+								<a href="{{ route('persona.show', $persona) }}">
+									<div class="widget-user-header bg-info">
+
+										<h3 class="widget-user-username">{{ $persona->nombre }} {{ $persona->apellido }}</h3>
+
+										<h5 class="widget-user-desc">Founder &amp; CEO</h5>
+									</div>
+									<div class="widget-user-image">
+										<img class="img-circle elevation-2" src="img/{{$persona->foto}}" alt="User Avatar">
+									</div>
+								</a>
+								<div class="card-footer">
+									<div class="row">
+										<div class="col-sm-4 border-right">
+											<div class="description-block">
+												<h5 class="description-header">3,200</h5>
+												<span class="description-text">SALES</span>
+											</div>
+										</div>
+										<div class="col-sm-4 border-right">
+											<div class="description-block">
+												<h5 class="description-header">13,000</h5>
+												<span class="description-text">FOLLOWERS</span>
+											</div>
+										</div>
+										<div class="col-sm-4">
+											<div class="description-block">
+												<h5 class="description-header">35</h5>
+												<span class="description-text">PRODUCTS</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						@endforeach
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</div>
+</div>
 @stop
 
 @section('css')
-	
+
 @stop
 
 @section('js')
-	
+
 @stop
