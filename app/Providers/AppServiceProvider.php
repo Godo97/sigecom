ec9ede0af0c2bfe05b2a02cfd9844c51d9a2c136
bdtem07477 <miguel.godoy@bt.gob.ve>
2023-09-22 18:15:19 -0400

    Add publish

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        $this->publishes([
+        'node_modules/gijgo' => public_path('plugins/gijgo'),
+        'node_modules/chart.js' => public_path('plugins/chart.js'),
+        'node_modules/axios/dist' => public_path('plugins/axios'),
+    ], 'npm');
     }
 }
